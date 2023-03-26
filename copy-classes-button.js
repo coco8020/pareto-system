3e8a15560d899adf8144e38428e52a33024e67d5
8020 <95773341+8020admin@users.noreply.github.com>
2023-03-26 01:21:26 +0000

    Json added

---
+++ copy-classes-button.js
@@ -0,0 +1,31 @@
+let webflowJSON = {
+    type: "@webflow/XscpData",
+    payload: {
+      nodes: [
+        {
+          _id: "4309a440-ade2-2a1f-5a96-396a84397c5b",
+          type: "Block",
+          tag: "div",
+          classes: [],
+          children: ["4309a440-ade2-2a1f-5a96-396a84397c5c"],
+          data: { text: true, tag: "div" }
+        },
+        {
+          _id: "4309a440-ade2-2a1f-5a96-396a84397c5c",
+          text: true,
+          v: "Classes added."
+        }
+      ],
+      styles: [],
+      assets: [],
+      ix1: [],
+      ix2: { interactions: [], events: [], actionLists: [] }
+    },
+    meta: {
+      unlinkedSymbolCount: 0,
+      droppedLinks: 0,
+      dynBindRemovedCount: 0,
+      dynListBindRemovedCount: 0,
+      paginationRemovedCount: 0
+    }
+  };
